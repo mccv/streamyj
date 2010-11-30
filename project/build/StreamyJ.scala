d8fedee7ed23185f09a17afe85a5a89b7208dc24
mmcbride <mccv@twitter.com>
2010-11-29 16:05:11 -0800

    setting this up to publish and cross-build

--- project/build/StreamyJ.scala
+++ project/build/StreamyJ.scala
@@ -1,8 +1,16 @@
 import sbt._
+import com.twitter.sbt._
 
-class StreamyJProject(info: ProjectInfo) extends DefaultProject(info)
+class StreamyJProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher
 {
   val jackson = "org.codehaus.jackson" % "jackson-core-asl" % "1.5.2"
-  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1"
   val objenesis = "org.objenesis" % "objenesis" % "1.1"
+  val specs     = buildScalaVersion match {
+    case "2.7.7" => "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
+    case _ => "org.scala-tools.testing" %% "specs" % "1.6.5" % "test"
+  }
+
+  override def disableCrossPaths = false
+
+  override def subversionRepository = Some("http://svn.local.twitter.com/maven-public")
 }
